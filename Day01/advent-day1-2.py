d42f39a767609fd50888a2650bdf8c53163049b9
Tatjana Sidorenko <tasidonya@gmail.com>
2023-12-02 12:00:56 +0000

    redid the solution to day1 part 2

--- Day01/advent-day1-2.py
+++ Day01/advent-day1-2.py
@@ -1,63 +1,42 @@
-import re 
-
-filepath = 'inputs.txt'
-# filepath = 'puzzle-input.txt'
+# filepath = 'inputs.txt'
+filepath = 'puzzle-input.txt'
 
 def read_file_lines():
     file1 = open(filepath, 'r')
     Lines = file1.readlines()
     return map(lambda x:x.strip(), Lines) # Strips the newline character
 
-def fused_number_check(input_str):
-    is_fused = False
-    fusion_value = ""
-    fused_list = {'oneight': (1, 8), 'threeight': (3, 8), 'fiveight': (5, 8), 'nineight': (9, 8)}
-
-    for item in fused_list:
-        if item in input_str:
-            is_fused = True
-            fusion_value = fused_list[item]
-
-    return (is_fused, fusion_value)
 
 def convert_line_to_digits(input_line):
-    digits_as_letters = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 'zero':0}
-
-
-    if (input_line == "oneight"):
-        return [1, 8]
-
-    built_string = ""
+    digits_as_letters = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9', 'zero':'0'}
     final_digits_list = []
+
+    counter = 0
     for char in input_line:
         if char.isdigit():
-            final_digits_list.append(int(char))
+            final_digits_list.append(char)
         else:
-            built_string += char
-            print(built_string)
-            fused_number = fused_number_check(built_string)
-            if fused_number[0]:
-                final_digits_list.append(fused_number[1])
-                built_string = ""
-            elif built_string in digits_as_letters:
-                final_digits_list.append(digits_as_letters[built_string])
-                built_string = ""
-    if len(built_string) > 4:
-        for char in built_string:
-            
+            for digit in digits_as_letters:
+                if input_line[counter:].startswith(digit):
+                    final_digits_list.append(digits_as_letters[digit])
+
+        counter += 1
+
 
     return final_digits_list
-            
 
 def main():
 
     Lines_no_new_line = read_file_lines()
 
+    final_sum = 0
     for line in Lines_no_new_line:
-        print(line)
         line_as_digits = convert_line_to_digits(line)
-        print(line_as_digits)
+        cat_nums_list = line_as_digits[:1] + line_as_digits[-1:]
+        cat_nums_joined = int("".join(cat_nums_list))
+        final_sum += cat_nums_joined
 
+    print(final_sum)
 
 if __name__ == "__main__":
     main()
