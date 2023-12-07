657f842029e69edfdd1efa659d851c88f35cdb30
Tatjana Sidorenko <tasidonya@gmail.com>
2023-12-07 13:47:25 +0000

    Started attempting to multiprocess it

--- Day03/day3.py
+++ Day03/day3.py
@@ -1,25 +1,39 @@
 import re
-import numpy
-# import copy
+import multiprocessing.dummy as mp
 
 filepath = 'inputs.txt'
 # filepath = 'puzzle-input.txt'
 
+all_lines = []
+states = []
 
-def main():
-	all_lines = []
-	for line in open(filepath,'r').read().split('\n'):
-		all_lines.append(line)
+def is_char_a_symbol(input_char):
+	if input_char in '#/*-+&$=@%':
+		return True
+	else:
+		return False
 
+def populate_states():
 	# I am basing this off the assumption that all elements are the same size
 	# y length = len(all_lines[0])
 	# x length = len(all_lines)
 
-	a_line = [{"Blank":False} for i in range(len(all_lines[0]))]
-	print(len(all_lines))
-	all_lines_as_dict = [numpy.repeat([a_line], range(len(all_lines)))]
+	for x, line in enumerate(states):
+		for original_line in all_lines:
+			for y, char in enumerate(original_line):
+				print(y, " ", char,  " ", x, " ", line, " ", original_line)
+				states[x][y][char] = is_char_a_symbol(char)
 
-	print(all_lines_as_dict[0])
+def main():
+	for line in open(filepath,'r').read().split('\n'):
+		all_lines.append(line)
+
+	states = [[{}] * len(all_lines[0])] * 10
+	p = mp.Pool(4)
+	p.map(populate_states)
+
+	print(states)
+
 
 if __name__ == "__main__":
     main()
